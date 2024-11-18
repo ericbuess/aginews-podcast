c51bfb8f56c3745e217287a4b15878b09c8af36b
Eric Ciarla <ericciarla@yahoo.com>
2024-11-18 13:47:37 -0500

    Update getCronSources.ts

--- src/services/getCronSources.ts
+++ src/services/getCronSources.ts
@@ -1,9 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SECRET_KEY!);
+
 
 export async function getCronSources() {
   try {
